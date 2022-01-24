c6fe3592fb40e47b4927f292502e35f2f40f945f
Jenna <jr13114@my.bristol.ac.uk>
2022-01-24 22:19:07 +0100

    amend endpoints to reflect change of 16 clue wall

--- src/server.ts
+++ src/server.ts
@@ -61,8 +61,9 @@ app.post("/wall", async (req, res) => {
     explanation4,
   } = req.body;
   const dbResponse = await client.query(
-    "insert into wall (clue1, clue2, clue3, clue4, connection, explanation) values ($25, $1, $2, $3, $4, $5, $6), ($25, $7, $8, $9, $10, $11, $12), ($25, $13, $14, $15, $16, $17, $18), ($25, $19, $20, $21, $22, $23, $24)",
+    "insert into wall (user, clue11, clue12,clue13,clue14,connection1,explanation1,clue21,clue22,clue23,clue24,connection2,explanation2,clue31,clue32,clue33,clue34,connection3,explanation3,clue41,clue42,clue43,clue44,connection4,explanation4) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25)",
     [
+      user,
       clue11,
       clue12,
       clue13,
@@ -87,7 +88,6 @@ app.post("/wall", async (req, res) => {
       clue44,
       connection4,
       explanation4,
-      user,
     ]
   );
 });
