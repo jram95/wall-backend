b9fcdb9d82392b4638e2660dd2c9e11259f28569
Jenna <jr13114@my.bristol.ac.uk>
2022-02-16 10:29:43 +0100

    test post endpoint

--- src/server.ts
+++ src/server.ts
@@ -44,63 +44,60 @@ app.get("/recent-walls", async (req, res) => {
 });
 
 app.post("/wall", async (req, res) => {
-  const {
-    user,
-    clue11,
-    clue12,
-    clue13,
-    clue14,
-    connection1,
-    explanation1,
-    clue21,
-    clue22,
-    clue23,
-    clue24,
-    connection2,
-    explanation2,
-    clue31,
-    clue32,
-    clue33,
-    clue34,
-    connection3,
-    explanation3,
-    clue41,
-    clue42,
-    clue43,
-    clue44,
-    connection4,
-    explanation4,
-  } = req.body;
-  const dbResponse = await client.query(
-    "insert into wall (user, clue11, clue12,clue13,clue14,connection1,explanation1,clue21,clue22,clue23,clue24,connection2,explanation2,clue31,clue32,clue33,clue34,connection3,explanation3,clue41,clue42,clue43,clue44,connection4,explanation4) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25)",
-    [
-      user,
+  try {
+    const {
+      username,
       clue11,
       clue12,
       clue13,
       clue14,
       connection1,
-      explanation1,
       clue21,
       clue22,
       clue23,
       clue24,
       connection2,
-      explanation2,
       clue31,
       clue32,
       clue33,
       clue34,
       connection3,
-      explanation3,
       clue41,
       clue42,
       clue43,
       clue44,
       connection4,
-      explanation4,
-    ]
-  );
+    } = req.body;
+    const dbResponse = await client.query(
+      "insert into wall (username, clue11, clue12,clue13,clue14,connection1,clue21,clue22,clue23,clue24,connection2,clue31,clue32,clue33,clue34,connection3,clue41,clue42,clue43,clue44,connection4) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21) returning *",
+      [
+        username,
+        clue11,
+        clue12,
+        clue13,
+        clue14,
+        connection1,
+        clue21,
+        clue22,
+        clue23,
+        clue24,
+        connection2,
+        clue31,
+        clue32,
+        clue33,
+        clue34,
+        connection3,
+        clue41,
+        clue42,
+        clue43,
+        clue44,
+        connection4,
+      ]
+    );
+    res.status(200).json({ status: "successful" });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 //Start the server on the given port
